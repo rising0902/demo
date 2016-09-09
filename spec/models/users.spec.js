941d169ac9b8d84cea3b4e8a684976e283218df8
s-hata <shinichi.hatano@ctc-g.co.jp>
2016-09-09 17:35:03 +0900

    add unit test.

--- spec/models/users.spec.js
+++ spec/models/users.spec.js
@@ -4,4 +4,8 @@ describe("users", function() {
   it("authenticate is success.", function() {
     expect(users.authenticate("user", "user")).toBeTruthy();
   });
+
+  it("authenticate is error.", function() {
+    expect(users.authenticate("user", "ng")).toBeFalsy();
+  });
 });
